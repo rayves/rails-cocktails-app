8eb6d4efc8fc8590fcbf4733e2bf647b9d0cd6f6
rayves <vo.raymond.v@outlook.com>
2022-03-17 03:14:31 +1100

    added coloring to view cards

--- app/helpers/cocktails_helper.rb
+++ app/helpers/cocktails_helper.rb
@@ -6,4 +6,26 @@ module CocktailsHelper
             return "Create"
         end
     end
+
+    def select_border_color(id)
+        colour = "%06x" % (rand * 0xffffff)
+        case id
+        when 1
+            return "border-top: 10px solid #935678"
+        when 2
+            return "border-top: 10px solid #abc472"
+        when 3
+            return "border-top: 10px solid #9D702E"
+        when 4
+            return "border-top: 10px solid #d29062"
+        when 5
+            return "border-top: 10px solid #ffb600"
+        when 6
+            return "border-top: 10px solid #b7621b"
+        when 7
+            return "border-top: 10px solid #d7d9d0"
+        else
+            return "border-top: 10px solid ##{colour}"
+        end
+    end
 end
